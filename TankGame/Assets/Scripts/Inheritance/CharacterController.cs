b455a5160f0dd79168034ae1461fc111270c5393
Miles Meacham <littlemeacham@gmail.com>
2015-11-07 12:31:10 -0700

    Working on inheritance for the characters

---
+++ TankGame/Assets/Scripts/Inheritance/CharacterController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float health;
+	public float maxHealth;
+	public float minHealth;
+	public float damage;
+	public float moveSpeed;
+	public float jumpForce;
+	public float jumpTime;
+
+
+
+	void character () {
+		health = 10;
+		maxHealth = 10;
+		minHealth = 0;
+		damage = 5;
+		moveSpeed = 5;
+		jumpForce = 0.2f;
+		jumpTime = 2;
+
+	}
+
+	protected void addHealth (float healthToAdd)
+	{
+		health += healthToAdd;
+		if (health > maxHealth)
+			health = maxHealth;
+	}
+
+	public void removeHealth (float healthToRemove)
+	{
+		health -= healthToRemove;
+		if (health < healthToRemove)
+			health = minHealth;
+	}
+
+	protected void addMaxHealth (float maxHealthToAdd)
+	{
+		maxHealth += maxHealthToAdd;
+
+	}
+
+	public void removeMaxHealth (float maxHealthToRemove)
+	{
+		maxHealth -= maxHealthToRemove;
+		if (health > maxHealth)
+			health = maxHealth;
+	}
+
+
+
+
+
+
+	protected float getHealth () 
+	{
+		return health;
+	}
+
+	protected float getDamage () 
+	{
+		return damage;
+	}
+
+	float getMoveSpeed () 
+	{
+		return moveSpeed;
+	}
+
+	float getJumpForce () 
+	{
+		return jumpForce;
+	}
+
+	float getJumpTime () 
+	{
+		return jumpTime;
+	}
+
+
+
+}
---
+++ TankGame/Assets/Scripts/Inheritance/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : CharacterController {
+
+
+
+	public float playerHealth = 20;
+	public float playerDamage = 5;
+
+	// Use this for initialization
+	void Start () {
+		health = playerHealth;
+		maxHealth = playerHealth;
+		damage = playerDamage;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.K))
+		    base.removeHealth(5f);
+		if (Input.GetKeyDown (KeyCode.L))
+			base.addHealth(5f);
+		if (Input.GetKeyDown (KeyCode.P))
+			base.addMaxHealth(5);
+		if (Input.GetKeyDown (KeyCode.O))
+			base.removeMaxHealth(5);
+		
+	}
+
+
+
+}
